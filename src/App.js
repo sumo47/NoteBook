a0c2f3d86673c32eb43d55c5f22d254c2cbaa4ad
sumit kumar <sumokhan76342@gmail.com>
2023-07-01 17:44:51 +0530

    integrate bootstrap , setup routers

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
